8321f37295f50da4262ce75586d0f9a5453b69c8
Suhail <suhailece@gmail.com>
2024-06-14 11:20:49 -0700

    update copyconfig.py

--- ociotools/scripts/testapi.py
+++
@@ -1,7 +0,0 @@
-import PyOpenColorIO as ocio
-
-blender_config_path = '../blender/config.ocio'
-blender_config = ocio.Config.CreateFromFile(blender_config_path)
-
-active_displays = blender_config.getActiveDisplays().split(', ')
-print(active_displays)
